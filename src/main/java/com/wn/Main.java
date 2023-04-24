ffae0042d04361ce3298d6c3f353573c9f3ad49e
taozhe <1447854534@qq.com>
2023-04-24 11:45:21 +0800

    add hello

--- src/main/java/com/wn/Main.java
+++ src/main/java/com/wn/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("傻狗刘庆悦");
+        System.out.println("大傻狗");
     }
 }
