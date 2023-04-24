0e893825d1d5c843ffa2fbd250e0d8ba741c9411
taozhe <1447854534@qq.com>
2023-04-24 14:28:54 +0800

    修改

--- src/main/java/com/wn/Main.java
+++ src/main/java/com/wn/Main.java
@@ -1,12 +1,14 @@
 package com.wn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   //指定当前项目为一个springboot项目
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("傻狗刘庆悦");
-        System.out.println("大傻狗");
-        System.out.println("小傻狗");
-         System.out.println("刘庆悦");
+
+        //作为一个Spring项目运行
+        SpringApplication.run(Main.class,args);
+
     }
 }
