944c3033a329de651cf5022d54300d2bc1ad682e
taozhe123 <131635758+taozhe123@users.noreply.github.com>
2023-04-24 11:49:56 +0800

    Update Main.java

--- src/main/java/com/wn/Main.java
+++ src/main/java/com/wn/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("傻狗刘庆悦");
         System.out.println("大傻狗");
+        System.out.println("小傻狗");
     }
 }
